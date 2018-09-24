b235b541dcd6fe89b01676fbf3986887ef2a3c87
Shashikant86 <shashikant.jagtap@icloud.com>
2018-09-24 22:11:42 +0100

    Initial Commit

---
+++ XCUITest101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCUITest101
+//
+//  Created by Shashikant Jagtap on 24/09/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
